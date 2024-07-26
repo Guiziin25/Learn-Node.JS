a91fe5c71b0107427b76b906d95795c5aa541753
Guilherme <gaandrade25@gmail.com>
2024-07-25 21:23:09 -0300

    Database connection
    The idea is to make a blog where I can control the posts. Using the ExpressJS framework where the connection to the MySQL database is made through the ORM Sequelize.

---
+++ test.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const handlebars = require('express-handlebars')
+const Sequelize = require('sequelize')
+
+
+// Config
+    // Template Engine
+        app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+
+    // Conexão com banco de dados Mysql
+    const sequelize = new Sequelize('db_sistema', 'root', 'biabia2525', {
+        host: "localhost",
+        dialect: 'mysql'
+    })
+
+app.listen(8081, function(){
+    console.log("Servidor está funcionand!");
+});
